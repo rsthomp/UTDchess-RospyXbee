d52e48db0b824f10048ae628fb841a197754403d
rachael <rachaelst@mac.com>
2015-07-06 17:23:57 -0500

    UTDchess-RospyXbee

--- src/scripts/cmd_vel_listener.py
+++ src/scripts/cmd_vel_listener.py
@@ -1,115 +1,181 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
-
+import sys
 import rospy
 import serial
 import struct
 import binascii
-from geometry_msgs.msg import Twist
+import time
+from teleop_twist_keyboard.msg import Command
+
 from xbee import ZigBee
 
 xbee = None
-XBEE_ADDR_LONG = '\x00\x13\xA2\x00\x40\x31\x56\x46'
-XBEE_ADDR_SHORT = '\x10\x52'
-DEVICE = '/dev/ttyUSB0'
+XBEE_ADDR_LONG = '\x00\x13\xA2\x00\x40\x86\x96\x4F'
+XBEE_ADDR_SHORT = '\xFF\xFE'
+DEVICE = '/dev/tty.usbserial-A603HA9K'
+#Each bot will have an addr long, addr short, and id stored. Coordinator is always id 0.
+bot_array = []
+final_bot_array = []
 
 
-def print_data(data):
-    rospy.loginfo("XBee Response: %s" % data)
+def find_bots():
+    #The coordinator broadcasts a "Node Discover" AT command and records the addresses recieved, I suspect
+    #Add coordinator first, then add on each bot as it responds.
+    #robot id command
+    
+    global xbee
+    
+    ser = serial.Serial(DEVICE, 57600)
+    xbee = ZigBee(ser)
 
-def prepare_data(msg):
-    linear = 0;
-    angular = 0;
+    try:
+        print("Searching for bots...")
+        xbee.at(
+            dest_addr_long = XBEE_ADDR_LONG,
+            dest_addr = XBEE_ADDR_SHORT,
+            command = 'ND'
+            )
+        timeout = time.time() + 30
+        num_of_robots = 0
+        while timeout > time.time():
+            dict = xbee.wait_read_frame()
+            if dict == None:
+                break
+            bot_array.append(parse_ND(dict))
+            print "Response: %r " % bot_array[num_of_robots]
+            num_of_robots += 1
+    except KeyboardInterrupt, SerialException:
+        sys.exit(0)
 
-    if msg.linear.x > 0:
-        linear = 1
-    elif msg.linear.x < 0:
-        linear = 2
+def get_bot_id(addr_long, addr_short):
+    #STILL NEEDS WORK
+    data = struct.pack('c', str(unichr(2)))
+    for item in bot_array:
+        addr_long = hex_to_addr(item['addr_long'])
+        addr_short = hex_to_addr(item['addr_short'])
+        xbee.tx(
+            dest_addr_long = addr_long,
+            dest_addr = addr_short,
+            data=data,
+            )
+        ans = xbee.wait_read_frame()
+        if ans == None:
+            print "Could not retrieve bot id"
+        print ans[6:12]
 
-    if msg.angular.z > 0:
-        angular = 2
-    elif msg.angular.z < 0:
-        angular = 1
 
-    data = struct.pack('BBB', 1, linear, angular)
+def search_for_bot(id):
+    #Takes the xbee ID, returns the xbee's dictionary
+    for dict in bot_array:
+        if dict['id'] == id:
+            return dict
+
+
+
+def prepare_move_cmd(msg):
+    code = str(unichr(4))
+    
+    #Packages the command message as binary for the API Frame
+    move = str(unichr(msg.direction))
+    speed = str(unichr(msg.magnitude))
+    turn = str(unichr(msg.turn))
+    accel = str(unichr(msg.accel))
+    data = struct.pack('ccccc', code, move, speed, turn, accel)
+    
     return data
 
 def cmd_vel_command(msg):
-    #a,b,c
-    #a = 1
-    #b = 1 - forward, b = -1 - backward
-    #c > 0 - turn right, c < 0 - turn left
-
-    data = prepare_data(msg)
+    data = prepare_move_cmd(msg)
 
     rospy.loginfo("Sending: %s" % binascii.hexlify(data))
-
+    
+    #Sends the message
     xbee.tx(
         dest_addr_long = XBEE_ADDR_LONG,
         dest_addr = XBEE_ADDR_SHORT,
         data=data,
     )
+    print parse(xbee.wait_read_frame())
 
 def callback(msg):
     rospy.loginfo("Received a /cmd_vel message!")
-    rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
-    rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
-    
     cmd_vel_command(msg)
-    
-    
+
 def listener():
     global xbee
 
-    ser = serial.Serial(DEVICE, 9600)
-    xbee = ZigBee(ser, callback=print_data)
+    ser = serial.Serial(DEVICE, 57600)
+    xbee = ZigBee(ser)
 
-    # in ROS, nodes are unique named. If two nodes with the same
+    # in ROS, nodes are uniquely named. If two nodes with the same
     # node are launched, the previous one is kicked off. The 
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'talker' node so that multiple talkers can
     # run simultaenously.
+    print "Ready to receive commands."
     rospy.init_node('cmd_vel_listener', anonymous=True)
-
-    rospy.Subscriber("/cmd_vel", Twist, callback)
+    
+    rospy.Subscriber("/cmd_hex", Command, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-
     xbee.halt()
     ser.close()
-        
+
+def parse(frame):
+    #Parses the transmit status for relevant info
+    info = {
+        'length': str(len(frame)/2),
+        'frame_id': frame[0:2],
+        'addr': frame[2:8],
+        'retry': frame[8:10],
+        'status': frame[10:12]
+        }
+    return info
+
+def parse_ND(frame):
+    #parses the node discovery response for relevant info
+    info = {
+        'length': str(len(frame)/2),
+        'frame_id': frame[0:2],
+        'command': frame[2:6],
+        'status': frame[6:8],
+        'addr_short': frame[8:12],
+        'addr_long': frame[12:28],
+        'id':  frame[39:40]
+        }
+    return info
+
+def hex_to_addr(adhex):
+    #Changes the hex address given by the dictionary
+    #to a format usable by the xbee. Works on long and short.
+    address = binascii.unhexlify(adhex)
+    return address
+
+
+def send_move_command(id, msg)
+    dict = search_for_bot(id)
+    data = prepare_move_cmd(msg)
+    
+    rospy.loginfo("Sending: %s" % binascii.hexlify(data))
+    
+    #Sends the message
+    xbee.tx(
+            dest_addr_long = unhexlify(dict['addr_long']),
+            dest_addr = unhexlify(dict['addr_short']),
+            data=data,
+            )
+    rospy.loginfo("   ")parse(xbee.wait_read_frame())
+
+
+
 if __name__ == '__main__':
-    listener()
+    find_bots()
+    #print search_for_bot('1')
+    #listener()
+    get_bot_id(XBEE_ADDR_LONG, XBEE_ADDR_SHORT)
+
+
+#tf package
+
+
